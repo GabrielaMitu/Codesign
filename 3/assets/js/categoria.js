37309de8f02a37d89f20f35f59b2411f656e8104
Gabriela Mitu <gabrielaym@al.insper.edu.br>
2019-09-13 14:00:19 -0300

    alows

---
+++ 3/assets/js/categoria.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+  let params = coDesExtract()
+  let value = params['key']
+
+  let db = coDesConnect('https://entrega1-42708.firebaseio.com/')
+
+  db.download('/', function(data) {
+
+    /*
+      NUNCA, JAMAIS, EM HIPÓTESE ALGUMA faça
+      replace em múltiplas tags se alguma
+      estiver dentro de outra! Veja o HTML
+      e note que não é o caso das abaixo.
+
+      É sério. Se fizer isso, você realmente
+      vai se arrepender. Você foi avisado.
+    */
+
+    context = data['portfolio'][value]
+    console.log(context)
+    coDesReplace('.csection1', context)
+
+    
+  })
+})
